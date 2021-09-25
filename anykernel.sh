0f90a33c955af338db8bbcd643ecca86e16d12f4
Sarthak Roy <sarthakroy2002@gmail.com>
2021-09-25 15:21:09 +0530

    init karashi

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,17 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=N.E.O.L.I.T_FoR_RMX2020/RMX2027
+kernel.string=N.E.O.L.I.T_FoR_Realme_Karashi
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=rmx2020
-device.name2=RMX2020
-device.name3=RMX2027
-device.name4=ZIPKUS
-device.name5=zipkus
+device.name1=karashi
+device.name2=RMX2185
+device.name3=RMX2189
+device.name4=KARASHI
+device.name5=rmx2185
 supported.versions=
 supported.patchlevels=
 '; } # end properties
