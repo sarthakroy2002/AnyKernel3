d8a50f13780d5df7d438f425910acc25a31329dc
Sarthak Roy <sarthakroy2002@gmail.com>
2022-04-26 15:40:47 +0530

    Add Cupida
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Id2d25462b4e760d16605fddd5e43ddcf5c86af77

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,17 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=N.E.O.L.I.T_FoR_RMX2020/RMX2027
+kernel.string=N.E.O.L.I.T_FOR_Cupida
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=rmx2020
-device.name2=RMX2020
-device.name3=RMX2027
-device.name4=ZIPKUS
-device.name5=zipkus
+device.name1=RMX3031
+device.name2=CUPIDA
+device.name3=cupida
+device.name4=
+device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
