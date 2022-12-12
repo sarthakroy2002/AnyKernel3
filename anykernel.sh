b37a07477af73a738ef861c2cc9723ee2fcf975e
Sarthak Roy <sarthakroy2002@gmail.com>
2022-12-12 21:05:37 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -7,7 +7,7 @@ properties() { '
 kernel.string=N.E.O.L.I.T_FoR_RMX2020/RMX2027
 do.devicecheck=1
 do.modules=1
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=rmx2020
