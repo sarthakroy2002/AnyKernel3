084eb32e082e2d6245124ccbaa4ecbac0763cd36
Sarthak Roy <sarthakroy2002@gmail.com>
2023-05-31 07:25:41 +0530

    Update!
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I21daba35d07876531bff56cda29b2e5a44bf675d

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=N.E.O.L.I.T_FoR_RMX2020/RMX2027
+kernel.string=neOliT for Realme Monet(RMX2020/RMX2027)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -13,8 +13,7 @@ do.cleanuponabort=0
 device.name1=rmx2020
 device.name2=RMX2020
 device.name3=RMX2027
-device.name4=ZIPKUS
-device.name5=zipkus
+device.name4=monet
 supported.versions=
 supported.patchlevels=
 '; } # end properties
