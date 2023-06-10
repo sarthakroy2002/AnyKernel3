a4c15cf9750906459f3ad9005824fe07db562a2f
prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>
2023-06-10 19:22:11 +0530

    fixup! Redo the supported device list as per the device tree
    Import and add the supported device list from the unified device tree for Realme Nemo/Sarter/Sala (RM6785). Also, clean up the unwated line breaks.
    
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=neOliT for Realme Wasabi(RM6785)
+kernel.string=neOliT for Realme Nemo/Sarter/Sala (RM6785)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -12,52 +12,57 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=RMX2001
 device.name2=RMX2151
-device.name3=RM6785
-device.name4=RMX2161
+device.name3=RMX2001L1
+device.name4=RMX2003L1
+device.name5=RMX2005L1
+device.name6=RMX2002L1
+device.name7=RMX2151L1
+device.name8=RMX2153L1
+device.name9=RMX2155L1
+device.name10=RMX2156L1
+device.name11=RMX2161L1
+device.name12=RMX2163L1
+device.name13=RM6785
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
-
+block=/dev/block/by-name/boot
+is_slot_device=0
+ramdisk_compression=auto
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. tools/ak3-core.sh;
-
+. tools/ak3-core.sh
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+set_perm_recursive 0 0 755 644 $ramdisk/*
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin
 
 ## AnyKernel install
-dump_boot;
+dump_boot
 
 # begin ramdisk changes
 
 # init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+backup_file init.rc
+replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack"
 
 # init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+backup_file init.tuna.rc
+insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0"
+append_file init.tuna.rc "bootscript" init.tuna
 
 # fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+backup_file fstab.tuna
+patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0"
+patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit"
+patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback"
+append_file fstab.tuna "usbdisk" fstab
 
 # end ramdisk changes
 
-write_boot;
+write_boot
 ## end install
-
